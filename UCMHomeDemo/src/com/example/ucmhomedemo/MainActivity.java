adf7be2566409940adcb59c93a723778a8ef1b1b
chengouxuan <chengouxuan@21cn.com>
2013-04-02 20:48:47 +0800

    this is log

--- UCMHomeDemo/src/com/example/ucmhomedemo/MainActivity.java
+++ UCMHomeDemo/src/com/example/ucmhomedemo/MainActivity.java
@@ -9,13 +9,14 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+//		setContentView(R.layout.activity_start_screen);
+		setContentView(R.layout.home);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.start_screen, menu);
 		return true;
 	}
 
