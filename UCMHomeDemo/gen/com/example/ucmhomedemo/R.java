ea58baf8c56a80f5c51bb07ae292e175c41d586e
chengouxuan <chengouxuan@21cn.com>
2013-04-02 20:41:03 +0800

    this is log

--- UCMHomeDemo/gen/com/example/ucmhomedemo/R.java
+++ UCMHomeDemo/gen/com/example/ucmhomedemo/R.java
@@ -27,10 +27,12 @@ public final class R {
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_start_screen=0x7f030000;
+        public static final int activity_main=0x7f030000;
+        public static final int activity_start_screen=0x7f030001;
     }
     public static final class menu {
-        public static final int start_screen=0x7f070000;
+        public static final int main=0x7f070000;
+        public static final int start_screen=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
