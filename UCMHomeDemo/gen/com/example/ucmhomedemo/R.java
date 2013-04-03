0ef1a1a341e0f66ec21d935bc8927d937830f711
chengouxuan <chengouxuan@21cn.com>
2013-04-03 11:10:01 +0800

    this is log

--- UCMHomeDemo/gen/com/example/ucmhomedemo/R.java
+++ UCMHomeDemo/gen/com/example/ucmhomedemo/R.java
@@ -10,10 +10,13 @@ package com.example.ucmhomedemo;
 public final class R {
     public static final class attr {
     }
+    public static final class bool {
+        public static final int scroll_view_fill_view_port=0x7f040000;
+    }
     public static final class color {
-        public static final int address_bar_background_color=0x7f050001;
-        public static final int bottom_bar_background_color=0x7f050002;
-        public static final int common_border_color=0x7f050000;
+        public static final int address_bar_background_color=0x7f060001;
+        public static final int bottom_bar_background_color=0x7f060002;
+        public static final int common_border_color=0x7f060000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -22,35 +25,36 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int common_padding=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int common_padding=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int start_screen=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int scrollView1=0x7f090000;
+        public static final int action_settings=0x7f0a0002;
+        public static final int addressBarLayout=0x7f0a0000;
+        public static final int bottomBarLayout=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_start_screen=0x7f030000;
         public static final int home=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int start_screen=0x7f080001;
+        public static final int main=0x7f090000;
+        public static final int start_screen=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int address_bar=0x7f060005;
-        public static final int app_name=0x7f060000;
-        public static final int bottom_button=0x7f060007;
-        public static final int hello_world=0x7f060002;
-        public static final int start_screen=0x7f060003;
-        public static final int table_common_text=0x7f060006;
-        public static final int title_activity_main=0x7f060004;
+        public static final int action_settings=0x7f070001;
+        public static final int address_bar=0x7f070005;
+        public static final int app_name=0x7f070000;
+        public static final int bottom_button=0x7f070007;
+        public static final int hello_world=0x7f070002;
+        public static final int start_screen=0x7f070003;
+        public static final int table_common_text=0x7f070006;
+        public static final int title_activity_main=0x7f070004;
     }
     public static final class style {
         /** 
@@ -63,10 +67,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
