2ee203ff9c25914d5a97e392d01695ab86dad9cd
chengouxuan <chengouxuan@21cn.com>
2013-04-03 16:26:48 +0800

    this is log

--- UCMHomeDemo/gen/com/example/ucmhomedemo/R.java
+++ UCMHomeDemo/gen/com/example/ucmhomedemo/R.java
@@ -17,6 +17,7 @@ public final class R {
         public static final int address_bar_background_color=0x7f060001;
         public static final int bottom_bar_background_color=0x7f060002;
         public static final int common_border_color=0x7f060000;
+        public static final int star_button_background_color=0x7f060003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -30,13 +31,44 @@ public final class R {
         public static final int common_padding=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int start_screen=0x7f020001;
+        public static final int bottom_button1=0x7f020000;
+        public static final int bottom_button2=0x7f020001;
+        public static final int bottom_button3=0x7f020002;
+        public static final int bottom_button4=0x7f020003;
+        public static final int bottom_button5=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int quick_button=0x7f020006;
+        public static final int smarturl_bookmark_ico=0x7f020007;
+        public static final int start_screen=0x7f020008;
+        public static final int url_bar_icon2=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0002;
+        public static final int action_settings=0x7f0a0019;
+        public static final int addressBarEditText=0x7f0a0005;
         public static final int addressBarLayout=0x7f0a0000;
-        public static final int bottomBarLayout=0x7f0a0001;
+        public static final int addressBarStarButton=0x7f0a0001;
+        public static final int bottomBarButton1=0x7f0a000c;
+        public static final int bottomBarButton2=0x7f0a000f;
+        public static final int bottomBarButton3=0x7f0a0012;
+        public static final int bottomBarButton4=0x7f0a0015;
+        public static final int bottomBarButton5=0x7f0a0018;
+        public static final int bottomBarButtonImage1=0x7f0a000b;
+        public static final int bottomBarButtonImage2=0x7f0a000e;
+        public static final int bottomBarButtonImage3=0x7f0a0011;
+        public static final int bottomBarButtonImage4=0x7f0a0014;
+        public static final int bottomBarButtonImage5=0x7f0a0017;
+        public static final int bottomBarItemLayout1=0x7f0a000a;
+        public static final int bottomBarItemLayout2=0x7f0a000d;
+        public static final int bottomBarItemLayout3=0x7f0a0010;
+        public static final int bottomBarItemLayout4=0x7f0a0013;
+        public static final int bottomBarItemLayout5=0x7f0a0016;
+        public static final int bottomBarLayout=0x7f0a0009;
+        public static final int button1=0x7f0a0003;
+        public static final int button2=0x7f0a0008;
+        public static final int globImage=0x7f0a0004;
+        public static final int quickButton=0x7f0a0006;
+        public static final int quickButtonOuterLayout=0x7f0a0007;
+        public static final int starButton=0x7f0a0002;
     }
     public static final class layout {
         public static final int activity_start_screen=0x7f030000;
@@ -48,13 +80,17 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f070001;
-        public static final int address_bar=0x7f070005;
+        public static final int address_bar=0x7f070006;
         public static final int app_name=0x7f070000;
-        public static final int bottom_button=0x7f070007;
+        public static final int bottom_button=0x7f070008;
+        public static final int button=0x7f070009;
+        public static final int common_button_string=0x7f07000a;
+        public static final int empty_description=0x7f070004;
         public static final int hello_world=0x7f070002;
         public static final int start_screen=0x7f070003;
-        public static final int table_common_text=0x7f070006;
-        public static final int title_activity_main=0x7f070004;
+        public static final int table_common_text=0x7f070007;
+        public static final int title_activity_main=0x7f070005;
+        public static final int type_in_address_hint=0x7f07000b;
     }
     public static final class style {
         /** 
--- UCMHomeDemo/src/com/example/ucmhomedemo/MainActivity.java
+++ UCMHomeDemo/src/com/example/ucmhomedemo/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import android.view.View;
+import android.app.AlertDialog;
+
+
 public class MainActivity extends Activity {
 
 	@Override
@@ -18,6 +22,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.start_screen, menu);
 		return true;
+	}
+	
+	public void anyButtonClicked(View senderView) {
+		
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(senderView.toString());
+        AlertDialog dialog = builder.create();
+        dialog.show();
 	}
 
 }
